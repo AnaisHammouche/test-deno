72934ec351bbc04945f1ebdc9217867d8be701d8
Anais <anais.hammouche@edu.itescia.fr>
2021-04-21 16:14:52 +0200

    tests

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,39 +1,17 @@
 // TODO: modify the body object!
-let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
 
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
 
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>
-`
-
-export async function handler (req: object) {
-  return {
-    statusCode: 200,
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    }
-    ,
-    body
-  }
-}
+// export async function handler (req: object) {
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       'content-type': 'text/html; charset=utf8',
+//       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+//     }
+//     ,
+//     body
+//   }
+// }
 
 // Example responses
 
@@ -49,10 +27,10 @@ export async function handler (req: object) {
 // }
 
 
-/* Respond with successful resource creation
+/* Respond with successful resource creation*/
 export async function handler (req: object) {
   return {
-    statusCode: 201,
+    statusCode: 200,
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
@@ -60,7 +38,7 @@ export async function handler (req: object) {
     body: JSON.stringify({ok: true})
   }
 }
-*/
+
 
 /* Deliver client-side JS
 export async function handler (req: object) {
