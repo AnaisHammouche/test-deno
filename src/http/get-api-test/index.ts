b710d91bad2d1298769a2e356428876b06717ddd
Anais <anais.hammouche@edu.itescia.fr>
2021-04-22 09:38:03 +0200

    test

--- src/http/get-api-test/index.ts
+++ src/http/get-api-test/index.ts
@@ -32,6 +32,7 @@ export async function handler (req: object) {
   return {
     statusCode: 200,
     headers: {
+      'Access-Control-Allow-Origin': '*',
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
